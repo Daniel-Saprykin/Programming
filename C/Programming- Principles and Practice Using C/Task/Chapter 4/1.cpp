54bbd0c0587ba8a0d1d62f1ffce6748208d1fcd4
Daniel <40882306+Daniel-Saprykin@users.noreply.github.com>
2018-07-15 21:45:32 +0300

    Update 1.cpp

--- C/Programming- Principles and Practice Using C/Task/Chapter 4/1.cpp
+++ C/Programming- Principles and Practice Using C/Task/Chapter 4/1.cpp
@@ -1,1 +1,26 @@
+// Напишите программу, содержащую цикл while, в котором считываются и выводятся на экран два числа типа int 
+// для выхода из программы используйте символ '|'.
 
+#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	int val1 = 0, val2 = 0;
+
+	while (cin >> val1 >> val2)
+	{
+		cout << "Значение 1: " << val1 << endl;
+		cout << "Значение 2: " << val2 << endl;
+		cout << endl;
+	}
+
+
+	system("pause");
+	return 0;
+}
